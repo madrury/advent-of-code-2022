048f6a6d3c082d68360ad3e778b88a53a1103032
Matthew Drury <matthew.drury.83@gmail.com>
2022-12-27 15:36:42 -0800

    Day 22 complete.

--- day22/solution.py
+++ day22/solution.py
@@ -1,4 +1,4 @@
-from aocd import get_data # type: ignore
+from aocd import get_data  # type: ignore
 from typing import List, Tuple, Dict, Callable, Optional, Literal
 from itertools import product
 from dataclasses import dataclass
@@ -9,15 +9,18 @@ class Terrain(Enum):
     OPEN = 0
     BLOCKED = 1
 
+
 class Side(Enum):
     RIGHT = 0
     TOP = 1
     LEFT = 2
     BOTTOM = 3
 
+
 class Glueing(Enum):
     MATCHING = 0
     REVERSED = 1
+
 
 class Facing(Enum):
     RIGHT = 0
@@ -30,6 +33,7 @@ SquareId = int
 Coord = Tuple[int, int]
 LocalCoord = Tuple[int, int]
 
+
 @dataclass
 class Square:
     id: SquareId
@@ -37,13 +41,18 @@ class Square:
     slen: int
     map: List[List[Terrain]]
 
-GlueingPattern = Dict[Tuple[SquareId, Side], Tuple[SquareId, Side, Glueing]]
+
+NClockwiseRotations = int
+GlueingPattern = Dict[
+    Tuple[SquareId, Side], Tuple[SquareId, Side, Glueing, NClockwiseRotations]
+]
+
 
 def symmetrize(d: GlueingPattern) -> GlueingPattern:
     r = {}
     for k, v in d.items():
         r[k] = v
-        r[(v[0], v[1])] = (k[0], k[1], v[2])
+        r[(v[0], v[1])] = (k[0], k[1], v[2], (4 - v[3]) % 4)
     return r
 
 
@@ -51,18 +60,19 @@ class Rotation(Enum):
     CLOCKWISE = 0
     COUNTERCLOCKWISE = 1
 
+
 # Lookup tables for translating rotations into new directions.
 CLOCKWISE_ROTATIONS = {
     Facing.RIGHT: Facing.DOWN,
     Facing.DOWN: Facing.LEFT,
     Facing.LEFT: Facing.UP,
-    Facing.UP: Facing.RIGHT
+    Facing.UP: Facing.RIGHT,
 }
 COUNTERCLOCKWISE_ROTATIONS = {
     Facing.RIGHT: Facing.UP,
     Facing.UP: Facing.LEFT,
     Facing.LEFT: Facing.DOWN,
-    Facing.DOWN: Facing.RIGHT
+    Facing.DOWN: Facing.RIGHT,
 }
 
 
@@ -70,6 +80,7 @@ COUNTERCLOCKWISE_ROTATIONS = {
 class Position:
     sqid: SquareId
     coord: LocalCoord
+
 
 @dataclass
 class Player:
@@ -81,17 +92,15 @@ class World:
     """The game world. Represented as a bunch of squares glued together at their
     boundaries.
     """
+
     def __init__(
-        self,
-        squares: Dict[SquareId, Square],
-        pattern: GlueingPattern,
-        player: Player
+        self, squares: Dict[SquareId, Square], pattern: GlueingPattern, player: Player
     ):
         self.squares = squares
         self.pattern = pattern
         self.player = player
 
-    def execute(self, instructions: List['Instruction'], show=False):
+    def execute(self, instructions: List["Instruction"], show=False):
         for instruction in instructions:
             match instruction:
                 case Instruction.CLOCKWISE:
@@ -109,83 +118,125 @@ class World:
                 input()
 
     def password(self) -> int:
-        globalrow = self.squares[self.player.position.sqid].coord[1] + self.player.position.coord[0]
-        globalcol = self.squares[self.player.position.sqid].coord[0] + self.player.position.coord[1]
-        facing = {
-            Facing.RIGHT: 0, Facing.DOWN: 1, Facing.LEFT: 2, Facing.UP: 3
-        }[self.player.facing]
-        print(globalrow, globalcol, facing)
-        return 1000*(globalrow + 1) + 4*(globalcol + 1) + facing
-
+        globalrow = (
+            self.squares[self.player.position.sqid].coord[1]
+            + self.player.position.coord[0]
+        )
+        globalcol = (
+            self.squares[self.player.position.sqid].coord[0]
+            + self.player.position.coord[1]
+        )
+        facing = {Facing.RIGHT: 0, Facing.DOWN: 1, Facing.LEFT: 2, Facing.UP: 3}[
+            self.player.facing
+        ]
+        return 1000 * (globalrow + 1) + 4 * (globalcol + 1) + facing
 
     def show(self):
-        TOKENS = {Terrain.OPEN: '.', Terrain.BLOCKED: '#'}
+        TOKENS = {Terrain.OPEN: ".", Terrain.BLOCKED: "#"}
         thissquare = [
             [TOKENS[terrain] for terrain in row]
             for row in self.squares[self.player.position.sqid].map
         ]
         ppos = self.player.position.coord
-        playertoken = {Facing.UP: '^', Facing.RIGHT: '>', Facing.LEFT: '<', Facing.DOWN: 'v'}[self.player.facing]
+        playertoken = {
+            Facing.UP: "^",
+            Facing.RIGHT: ">",
+            Facing.LEFT: "<",
+            Facing.DOWN: "v",
+        }[self.player.facing]
         thissquare[ppos[0]][ppos[1]] = playertoken
         for row in thissquare:
-            print(''.join(row))
+            print("".join(row))
 
     def rotate(self, rotation: Rotation):
         self.player.facing = self._rotate(rotation)
 
-    def _rotate(self, rotation: Rotation) -> Facing:
+    def _rotate(self, rotation: Rotation, ntimes: int = 1) -> Facing:
         match rotation:
             case Rotation.CLOCKWISE:
-                return CLOCKWISE_ROTATIONS[self.player.facing]
+                lookup = CLOCKWISE_ROTATIONS
+                # return CLOCKWISE_ROTATIONS[self.player.facing]
             case Rotation.COUNTERCLOCKWISE:
-                return COUNTERCLOCKWISE_ROTATIONS[self.player.facing]
+                lookup = COUNTERCLOCKWISE_ROTATIONS
+                # return COUNTERCLOCKWISE_ROTATIONS[self.player.facing]
+        result = self.player.facing
+        for _ in range(ntimes):
+            result = lookup[result]
+        return result
 
     def move(self):
-        self.player.position = self._move()
+        self.player = self._move()
 
-    def _move(self) -> Position:
+    def _move(self) -> Player:
         newcoord: LocalCoord
         match self.player.facing:
             case Facing.RIGHT:
-                newcoord = (self.player.position.coord[0], self.player.position.coord[1] + 1)
+                newcoord = (
+                    self.player.position.coord[0],
+                    self.player.position.coord[1] + 1,
+                )
             case Facing.LEFT:
-                newcoord = (self.player.position.coord[0], self.player.position.coord[1] - 1)
+                newcoord = (
+                    self.player.position.coord[0],
+                    self.player.position.coord[1] - 1,
+                )
             case Facing.UP:
-                newcoord = (self.player.position.coord[0] - 1, self.player.position.coord[1])
+                newcoord = (
+                    self.player.position.coord[0] - 1,
+                    self.player.position.coord[1],
+                )
             case Facing.DOWN:
-                newcoord = (self.player.position.coord[0] + 1, self.player.position.coord[1])
+                newcoord = (
+                    self.player.position.coord[0] + 1,
+                    self.player.position.coord[1],
+                )
         # Coordinates are still within the same square.
         samesquare = (
             0 <= newcoord[0] < self.squares[self.player.position.sqid].slen
             and 0 <= newcoord[1] < self.squares[self.player.position.sqid].slen
         )
         if samesquare:
-            newterrain = self.squares[self.player.position.sqid].map[newcoord[0]][newcoord[1]]
+            newterrain = self.squares[self.player.position.sqid].map[newcoord[0]][
+                newcoord[1]
+            ]
             match newterrain:
                 case Terrain.OPEN:
-                    return Position(self.player.position.sqid, newcoord)
+                    position = Position(self.player.position.sqid, newcoord)
+                    return Player(position, self.player.facing)
                 case Terrain.BLOCKED:
-                    return self.player.position
+                    return self.player
         # We've gone over an edge.
         # newsid, newside, glue: Tuple[SquareId, Side, Glueing]
         match self.player.facing:
             case Facing.RIGHT:
-                newsid, newside, glue = self.pattern[self.player.position.sqid, Side.RIGHT]
+                newsid, newside, glue, nrot = self.pattern[
+                    self.player.position.sqid, Side.RIGHT
+                ]
             case Facing.LEFT:
-                newsid, newside, glue = self.pattern[self.player.position.sqid, Side.LEFT]
+                newsid, newside, glue, nrot = self.pattern[
+                    self.player.position.sqid, Side.LEFT
+                ]
             case Facing.UP:
-                newsid, newside, glue = self.pattern[self.player.position.sqid, Side.TOP]
+                newsid, newside, glue, nrot = self.pattern[
+                    self.player.position.sqid, Side.TOP
+                ]
             case Facing.DOWN:
-                newsid, newside, glue = self.pattern[self.player.position.sqid, Side.BOTTOM]
+                newsid, newside, glue, nrot = self.pattern[
+                    self.player.position.sqid, Side.BOTTOM
+                ]
         return self._new_square_move(
-            self.player.position.coord,
-            self.player.facing,
-            newsid,
-            newside,
-            glue
+            self.player.position.coord, self.player.facing, newsid, newside, glue, nrot
         )
 
-    def _new_square_move(self, coord: LocalCoord, facing: Facing, newsquareid: SquareId, newside: Side, glue: Glueing) -> Position:
+    def _new_square_move(
+        self,
+        coord: LocalCoord,
+        facing: Facing,
+        newsquareid: SquareId,
+        newside: Side,
+        glue: Glueing,
+        nrot: int,
+    ) -> Position:
         # Coordinate on the edge of the square we just left.
         edgecoord: int
         match facing:
@@ -197,7 +248,7 @@ class World:
         # coordinate.
         N = self.squares[newsquareid].slen
         if glue == Glueing.REVERSED:
-            edgecoord = N - edgecoord
+            edgecoord = N - edgecoord - 1
         # Local coordinate in the new square we are entering.
         newlocalcoord: LocalCoord
         match newside:
@@ -213,63 +264,74 @@ class World:
         newterrain = self.squares[newsquareid].map[newlocalcoord[0]][newlocalcoord[1]]
         match newterrain:
             case Terrain.OPEN:
-                return Position(newsquareid, newlocalcoord)
+                position = Position(newsquareid, newlocalcoord)
+                facing = self._rotate(Rotation.CLOCKWISE, nrot)
+                return Player(position, facing)
             case Terrain.BLOCKED:
-                return self.player.position
+                return self.player
+
 
 def parse_data(data: str) -> Tuple[str, str]:
     mapdata: List[str] = []
     instructiondata: str
 
     readingmap = True
-    for line in data.split('\n'):
-        if line.strip() == '':
+    for line in data.split("\n"):
+        if line.strip() == "":
             readingmap = False
             continue
         if readingmap:
             mapdata.append(line)
         else:
             instructiondata = line.strip()
-    return '\n'.join(mapdata), instructiondata
+    return "\n".join(mapdata), instructiondata
+
 
 ReferencePoints = Dict[SquareId, Coord]
 
-def parse_map_data(data: str, N: int, referencepts: ReferencePoints) -> Dict[SquareId, Square]:
-    LOOKUP = {'.': Terrain.OPEN, '#': Terrain.BLOCKED}
+
+def parse_map_data(
+    data: str, N: int, referencepts: ReferencePoints
+) -> Dict[SquareId, Square]:
+    LOOKUP = {".": Terrain.OPEN, "#": Terrain.BLOCKED}
     squares = {id: Square(id, pt, N, []) for id, pt in referencepts.items()}
-    for (n, line), (id, pt) in product(enumerate(data.split('\n')), referencepts.items()):
+    for (n, line), (id, pt) in product(
+        enumerate(data.split("\n")), referencepts.items()
+    ):
         if pt[1] <= n < pt[1] + N:
-            squares[id].map.append([
-                LOOKUP[ch] for ch in line[pt[0]:(pt[0] + N)]
-            ])
+            squares[id].map.append([LOOKUP[ch] for ch in line[pt[0] : (pt[0] + N)]])
     return squares
+
 
 class Instruction(Enum):
     MOVE = 0
     CLOCKWISE = 1
     COUNTERCLOCKWISE = 2
 
+
 def parse_instruction_data(data: str) -> List[Instruction]:
     buffer: List[str] = []
     instructions: List[Instruction] = []
     for chr in data:
-        if chr in {'R', 'L'}:
+        if chr in {"R", "L"}:
             if buffer:
-                instructions.append((Instruction.MOVE, int(''.join(buffer))))
+                instructions.append((Instruction.MOVE, int("".join(buffer))))
                 buffer = []
             instructions.append(
-                {'R': Instruction.CLOCKWISE, 'L': Instruction.COUNTERCLOCKWISE}[chr]
+                {"R": Instruction.CLOCKWISE, "L": Instruction.COUNTERCLOCKWISE}[chr]
             )
         else:
             buffer.append(chr)
     if buffer:
-        instructions.append((Instruction.MOVE, int(''.join(buffer))))
+        instructions.append((Instruction.MOVE, int("".join(buffer))))
     return instructions
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     data = get_data(day=22, year=2022)
     mapdata, instructiondata = parse_data(data)
+
+    instructions = parse_instruction_data(instructiondata)
 
     SIDELEN = 50
     REFPTS = {
@@ -280,79 +342,45 @@ if __name__ == '__main__':
         4: (SIDELEN, 2*SIDELEN),
         5: (0, 3*SIDELEN)
     }
-    GLUEING_PATTERN = symmetrize({
-        (0, Side.TOP):  (4, Side.BOTTOM, Glueing.MATCHING),
-        (0, Side.LEFT): (1, Side.RIGHT, Glueing.MATCHING),
-        (1, Side.TOP):  (1, Side.BOTTOM, Glueing.MATCHING),
-        (1, Side.LEFT): (0, Side.RIGHT, Glueing.MATCHING),
-        (2, Side.TOP):  (0, Side.BOTTOM, Glueing.MATCHING),
-        (2, Side.LEFT): (2, Side.RIGHT, Glueing.MATCHING),
-        (3, Side.TOP):  (5, Side.BOTTOM, Glueing.MATCHING),
-        (3, Side.LEFT): (4, Side.RIGHT, Glueing.MATCHING),
-        (4, Side.TOP):  (2, Side.BOTTOM, Glueing.MATCHING),
-        (4, Side.LEFT): (3, Side.RIGHT, Glueing.MATCHING),
-        (5, Side.TOP):  (3, Side.BOTTOM, Glueing.MATCHING),
-        (5, Side.LEFT): (5, Side.RIGHT, Glueing.MATCHING)
+    squares = parse_map_data(mapdata, SIDELEN, REFPTS)
+
+    FLAT_GLUEING_PATTERN = symmetrize({
+        (0, Side.TOP):  (4, Side.BOTTOM, Glueing.MATCHING, 0),
+        (0, Side.LEFT): (1, Side.RIGHT, Glueing.MATCHING, 0),
+        (1, Side.TOP):  (1, Side.BOTTOM, Glueing.MATCHING, 0),
+        (1, Side.LEFT): (0, Side.RIGHT, Glueing.MATCHING, 0),
+        (2, Side.TOP):  (0, Side.BOTTOM, Glueing.MATCHING, 0),
+        (2, Side.LEFT): (2, Side.RIGHT, Glueing.MATCHING, 0),
+        (3, Side.TOP):  (5, Side.BOTTOM, Glueing.MATCHING, 0),
+        (3, Side.LEFT): (4, Side.RIGHT, Glueing.MATCHING, 0),
+        (4, Side.TOP):  (2, Side.BOTTOM, Glueing.MATCHING, 0),
+        (4, Side.LEFT): (3, Side.RIGHT, Glueing.MATCHING, 0),
+        (5, Side.TOP):  (3, Side.BOTTOM, Glueing.MATCHING, 0),
+        (5, Side.LEFT): (5, Side.RIGHT, Glueing.MATCHING, 0)
     })
 
-    squares = parse_map_data(mapdata, SIDELEN, REFPTS)
     start = Position(0, (0, 0))
-    world = World(squares, GLUEING_PATTERN, Player(start, Facing.RIGHT))
-
-    instructions = parse_instruction_data(instructiondata)
-
+    world = World(squares, FLAT_GLUEING_PATTERN, Player(start, Facing.RIGHT))
     world.execute(instructions, False)
     password = world.password()
-    print(f"The final password in {password}")
+    print(f"The final password for the flat map is {password}")
 
-#     mapdata = (
-# """        ...#
-#         .#..
-#         #...
-#         ....
-# ...#.......#
-# ........#...
-# ..#....#....
-# ..........#.
-#         ...#....
-#         .....#..
-#         .#......
-#         ......#."""
-#     )
-
-#     EXAMPLE_REFPTS = {
-#         0: (8, 0),
-#         1: (0, 4),
-#         2: (4, 4),
-#         3: (8, 4),
-#         4: (8, 8),
-#         5: (12, 8)
-#     }
-
-#     EXAMPLE_FLAT_GLUEING_PATTERN = symmetrize({
-#         (0, Side.TOP): (4, Side.BOTTOM, Glueing.MATCHING),
-#         (0, Side.LEFT): (0, Side.RIGHT, Glueing.MATCHING),
-#         (1, Side.TOP): (1, Side.BOTTOM, Glueing.MATCHING),
-#         (1, Side.LEFT): (3, Side.RIGHT, Glueing.MATCHING),
-#         (1, Side.LEFT): (3, Side.RIGHT, Glueing.MATCHING),
-#         (2, Side.TOP): (2, Side.BOTTOM, Glueing.MATCHING),
-#         (2, Side.LEFT): (1, Side.RIGHT, Glueing.MATCHING),
-#         (3, Side.TOP): (0, Side.BOTTOM, Glueing.MATCHING),
-#         (3, Side.LEFT): (2, Side.RIGHT, Glueing.MATCHING),
-#         (4, Side.TOP): (3, Side.BOTTOM, Glueing.MATCHING),
-#         (4, Side.LEFT): (5, Side.RIGHT, Glueing.MATCHING),
-#         (5, Side.TOP): (5, Side.BOTTOM, Glueing.MATCHING),
-#         (5, Side.LEFT): (4, Side.RIGHT, Glueing.MATCHING),
-#     })
-
-#     squares = parse_map_data(mapdata, 4, EXAMPLE_REFPTS)
-#     start = Position(0, (0, 0))
-#     world = World(squares, EXAMPLE_FLAT_GLUEING_PATTERN, Player(start, Facing.RIGHT))
-
-#     instructiondata = "10R5L5R10L4R5L5"
-#     instructions = parse_instruction_data(instructiondata)
-#     print(instructions)
-
-#     world.execute(instructions, True)
-#     password = world.password()
-#     print(f"The final password in {password}")
+    CUBE_GLUEING_PATTERN = symmetrize({
+        (2, Side.RIGHT):  (1, Side.BOTTOM, Glueing.MATCHING, 3), # a
+        (2, Side.LEFT):   (3, Side.TOP, Glueing.MATCHING, 3),    # b
+        (5, Side.RIGHT):  (4, Side.BOTTOM, Glueing.MATCHING, 3), # c
+        (1, Side.RIGHT):  (4, Side.RIGHT, Glueing.REVERSED, 2),  # d
+        (0, Side.LEFT):   (3, Side.LEFT, Glueing.REVERSED, 2),   # e
+        (0, Side.TOP):    (5, Side.LEFT, Glueing.MATCHING, 1),   # f
+        (5, Side.BOTTOM): (1, Side.TOP, Glueing.MATCHING, 0),    # g
+        (0, Side.RIGHT):  (1, Side.LEFT, Glueing.MATCHING, 0),
+        (0, Side.BOTTOM): (2, Side.TOP, Glueing.MATCHING, 0),
+        (2, Side.BOTTOM): (4, Side.TOP, Glueing.MATCHING, 0),
+        (3, Side.RIGHT):  (4, Side.LEFT, Glueing.MATCHING, 0),
+        (3, Side.BOTTOM): (5, Side.TOP, Glueing.MATCHING, 0)
+    })
+    start = Position(0, (0, 0))
+    world = World(squares, CUBE_GLUEING_PATTERN, Player(start, Facing.RIGHT))
+    world.execute(instructions, False)
+    password = world.password()
+    print(f"The final password for the cube map is {password}")
